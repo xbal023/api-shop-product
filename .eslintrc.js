237695144abf55331ffebf7f3d4bb60848e4478e
bolaxd <iqbalsheila619@gmail.com>
2023-04-03 01:13:10 +0700

    delete husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,39 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true
-  },
-  extends: 'standard-with-typescript',
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest'
-  },
-  plugins: ['@typescript-eslint'],
-  ignorePatterns: ['**/build/*', '**/node_modules/*', '**/public/*'],
-  rules: {}
-}
+	'env': {
+		'es2021': true,
+		'node': true
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended'
+	],
+	'overrides': [
+	],
+	'parser': '@typescript-eslint/parser',
+	'parserOptions': {
+		'ecmaVersion': 'latest',
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'@typescript-eslint'
+	],
+	'rules': {
+		'@typescript-eslint/ban-types': 'off',
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		]
+	}
+};
