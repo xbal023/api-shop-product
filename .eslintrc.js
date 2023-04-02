0e7cdf697c4f135105ac19a4bf14d79170a214ab
bolaxd <iqbalsheila619@gmail.com>
2023-04-02 21:07:48 +0700

    add eslint config + prettier

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: 'standard-with-typescript',
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  plugins: ["@typescript-eslint"],
+  ignorePatterns: ['**/build/*', '**/node_modules/*', '**/public/*'],
+  rules: {}
+}
